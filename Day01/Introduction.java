93944f9a12b387a7e865b503c7fbc16cb6d7d087
Swapnil Kadam <113785187+SwapnilKadamPatil@users.noreply.github.com>
2023-06-01 19:06:20 +0530

    Introduction.java

--- Day01/Introduction.java
+++ Day01/Introduction.java
@@ -1,1 +1,5 @@
-
+/*
+Java is a programming language and computing platform first released by Sun Microsystems in 1995 and is now operated by Oracle which releases a new version of this language every year.
+Java is a programming language and computing platform which serves as a general-purpose language for developers to create modular programs and reusable code. With its Write Once Run Anywhere principle,
+Java is now of the most widely used and recommended programming language for developing desktop as well as web applications in today’s digitized world.
+*/
