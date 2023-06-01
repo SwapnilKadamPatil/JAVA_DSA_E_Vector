d40a648b4faef85c6695b81b7433f28ee49681d4
Swapnil Kadam <113785187+SwapnilKadamPatil@users.noreply.github.com>
2023-06-01 19:47:46 +0530

    --
    --

---
+++ Day01/Sum_Two_Numbers.java
@@ -0,0 +1,20 @@
+package JavaDSA_Swapnil.com;
+
+import java.util.Scanner;
+
+public class Sum_Two_Numbers {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two number :");
+		
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.print("Result = ");
+		System.out.println(a+b);
+		
+
+	}
+
+}
