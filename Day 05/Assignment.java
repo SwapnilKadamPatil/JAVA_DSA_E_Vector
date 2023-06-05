d5f336e2702916b61d8ca204c6035886ce61dbaa
Swapnil Kadam <113785187+SwapnilKadamPatil@users.noreply.github.com>
2023-06-05 13:44:23 +0530

    Assignment.java

---
+++ Day 05/Assignment.java
@@ -0,0 +1,13 @@
+Programs for Practice
+
+1) Java Program to check whether a year is leap year or not using if-else.
+2) Java Program to check whether a given input is digit or not using if-else.
+3) Java Program to check whether a given input is alphabet or not using if-else.
+4) Java Program to check if a given input is a Digit or Alphabets or Special Character using if-else.
+5) Java Program to convert temperature from Celsius to Fahrenheit or vice versa using if-else. 
+6) Java Program to check whether a character is a vowel or consonant using if-else.
+7) Java Program to check Division on the basis of marks obtained by students. Percentage >= 60% : Division 1, Percentage >= 45% : Division 2, Percentage >= 30% : Division 3,
+Percentage < 30% : Fail
+8) Java Program to Calculate percentage and grade of students on the basis of marks obtained in 5 subjects Maths, Physics, Chemistry, English and Hindi. Marks of subject is 
+taken as an input: Percentage >= 90% : Grade A, Percentage >= 80% : Grade B, Percentage >= 70% : Grade C, Percentage >= 60% : Grade D, Percentage >= 40% : Grade E,
+Percentage < 40% : Grade F 
