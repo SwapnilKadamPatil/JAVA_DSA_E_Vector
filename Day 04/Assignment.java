a29473c5ee943ae9a6f6da5ab01ffbf42a1ec343
Swapnil Kadam <113785187+SwapnilKadamPatil@users.noreply.github.com>
2023-06-05 13:41:43 +0530

    Assignment.java
    -

---
+++ Day 04/Assignment.java
@@ -0,0 +1,16 @@
+Try to use All the methods of scanner class in your code
+
+//INPUT
+nextInt()
+nextFloat()
+nextDouble()
+nextByte()
+nextShort()
+nextLong()
+nextBoolean()
+nextLine()
+next().charAt(position)
+nextBigInteger()
+nextBigDecimal()
+
+Explore more about BufferReader class & Practice on Code
