cd2cf0a939cbeb2dbb5bc7d51aed60f664aec131
Swapnil Kadam <113785187+SwapnilKadamPatil@users.noreply.github.com>
2023-06-10 15:53:18 +0530

    charAt

---
+++ Day 07 /Strings /charAt.java
@@ -0,0 +1,15 @@
+//harAtString’s charAt method returns char value present at specified index. String is nothing but Sequence of char values and it 
+//starts with index 0.
+
+package String_Programs;
+
+public class CharAt 
+{
+	public static void main(String[] args) {
+		String str = "Java Developer";
+		 
+		System.out.println(" Char at index 5 is ="+ str.charAt(5));
+		System.out.println("Char at index 2 is ="+ str.charAt(2));
+		System.out.println("Char at index 7 is ="+ str.charAt(7));
+	}
+}
